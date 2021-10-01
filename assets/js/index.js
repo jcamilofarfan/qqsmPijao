567e49838dee756900714108d17c829c08727c55
Juan Camilo Farfan <farfan1976@hotmail.com>
2021-09-30 20:40:57 -0500

    basic game

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const name_ = document.getElementById('name');
+const challenge = document.getElementById('challenge');
+const submitButton = document.getElementById('submit-button');
+const gameForm = document.getElementById('game-form');
+
+const activateButton = () => {
+  if (name_.value && challenge.value) submitButton.removeAttribute('disabled');
+  else submitButton.setAttribute('disabled', true);
+}
+
+const setDataToGame = () => {
+  localStorage.setItem('name', name_.value);
+  localStorage.setItem('challenge', challenge.value);
+  window.location.href = 'game.html';
+}
+
+name_.onchange = function () { activateButton(); };
+challenge.onchange = function () { activateButton(); };
+gameForm.onsubmit = function (e) {
+  e.preventDefault();
+  setDataToGame();
+};
---
+++ assets/js/millonarie.js
@@ -0,0 +1,756 @@
+// questions
+const data = {
+  "games": [
+    {
+      "questions": [
+        // 1
+        {
+          "question": "Como se llama el Sol en Pijao:",
+          "content": [
+            "Tolaima",
+            "Taiba",
+            "Chiri ",
+            "Ta"
+          ],
+          "correct": 3
+        },
+        // 2
+        {
+          "question": "Como se llama el equilibrio que debemos mantener los Pijao:",
+          "content": [
+            "Entre el Sol y el Arcoiris",
+            "Chiri Chajua",
+            "Hielo y frio",
+            "El bien y el Mal"
+          ],
+          "correct": 1
+        },
+        // 3
+        {
+          "question": "Como era la tierra hace muchos Soles y Lunas",
+          "content": [
+            "Cubierta de agua",
+            "Desertica",
+            "Congelada",
+            "Como es ahora"
+          ],
+          "correct": 0
+        },
+        // 4
+        {
+          "question": "Como se les dice a lo primeros pijao",
+          "content": [
+            "Ta",
+            "Señores",
+            "Abuelos",
+            "Mohanes"
+          ],
+          "correct": 3
+        },
+        // 5
+        {
+          "question": "Que hacia Taiba sobre la tierra",
+          "content": [
+            "Miraba su reflejo",
+            "Bailaba",
+            "Jugaba",
+            "Estudiaba"
+          ],
+          "correct": 0
+        },
+        // 6
+        {
+          "question": "Como se le dice a la madre Tierra en dialecto pijao",
+          "content": [
+            "Botta Ima",
+            "Mama",
+            "Naturaleza",
+            "Tierra"
+          ],
+          "correct": 0
+        },
+        // 7
+        {
+          "question": "Que paso cuando ta vio a Botta Ima",
+          "content": [
+            "Se asusto",
+            "Se enamoro",
+            "Se rio",
+            "Se devolvio"
+          ],
+          "correct": 1
+        },
+        // 8
+        {
+          "question": "Que nos da el arcoiris:",
+          "content": [
+            "La lluvia",
+            "El sol",
+            "Los colores",
+            "Las plantas"
+          ],
+          "correct": 2
+        },
+        // 9
+        {
+          "question": "Por que Ta y Taiba se separaron",
+          "content": [
+            "Para seguir peliando",
+            "Por diversion",
+            "Para mantener el equilibrio",
+            "Por amor"
+          ],
+          "correct": 2
+        }
+      ]
+    },
+    {
+      "questions": [
+        // 1
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 2
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 3
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 4
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 5
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 6
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 7
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 8
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 9
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        }
+      ]
+    },
+    {
+      "questions": [
+        // 1
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 2
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 3
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 4
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 5
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 6
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 7
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 8
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 9
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        }
+      ]
+    },
+    {
+      "questions": [
+        // 1
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 2
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 3
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 4
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 5
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 6
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 7
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 8
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 9
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        }
+      ]
+    },
+    {
+      "questions": [
+        // 1
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 2
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 3
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 4
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 5
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 6
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 7
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 8
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 9
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        }
+      ]
+    },
+    {
+      "questions": [
+        // 1
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 2
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 3
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 4
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 5
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 6
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 7
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 8
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        },
+        // 9
+        {
+          "question": "Pendiente de pregunta",
+          "content": [
+            "Falso",
+            "Correcto",
+            "Falso ",
+            "Falso"
+          ],
+          "correct": 1
+        }
+      ]
+    }
+  ]
+}
+// DOM els
+const preStart = document.getElementById('pre-start');
+const start = document.getElementById('start');
+const game = document.getElementById('game');
+const gameOverEl = document.getElementById('game-over');
+const question = document.getElementById('question');
+const answer1 = document.getElementById('answer-one');
+const answer2 = document.getElementById('answer-two');
+const answer3 = document.getElementById('answer-three');
+const answer4 = document.getElementById('answer-four');
+
+const startSound = (id, loop) => {
+  const soundHandle = document.getElementById(id);
+  // canción de fondo repetir hasta el infinuito
+  if (loop) soundHandle.setAttribute('loop', loop);
+  soundHandle.play();
+}
+
+let levelGame = 0;
+let topLevel = 0;
+let challengeSelected = 0;
+
+const setGame = (dataGame = [], level = 0) => {
+  levelGame = level;
+  topLevel = dataGame.questions.length - 1;
+
+  if (levelGame === topLevel) {
+    gameOverEl.innerHTML = `Ganaste ${localStorage.getItem('name')}`;
+    gameOverEl.style.display = 'block';
+    game.style.display = 'none';
+    localStorage.removeItem('name');
+    localStorage.removeItem('challenge');
+    return;
+  }
+
+  setQuestion(dataGame.questions[level].question);
+  setAnswers(dataGame.questions[level].content, level);
+}
+
+const setQuestion = (q = '') => {
+  let string = q;
+  question.innerHTML = decodeURIComponent(escape(string));
+}
+
+const setAnswers = (answers = [], level) => {
+  // answer 1
+  answer1.setAttribute('level', level);
+  answer1.setAttribute('answer', 0);
+  const spans1 = answer1.getElementsByTagName('span');
+  const span1 = spans1[1];
+  span1.innerHTML = decodeURIComponent(escape(answers[0]));
+
+  // answer 2
+  answer2.setAttribute('level', level);
+  answer2.setAttribute('answer', 1);
+  const spans2 = answer2.getElementsByTagName('span');
+  const span2 = spans2[1];
+  span2.innerHTML = decodeURIComponent(escape(answers[1]));
+
+  // answer 3
+  answer3.setAttribute('level', level);
+  answer3.setAttribute('answer', 2);
+  const spans3 = answer3.getElementsByTagName('span');
+  const span3 = spans3[1];
+  span3.innerHTML = decodeURIComponent(escape(answers[2]));
+
+  // answer 4
+  answer4.setAttribute('level', level);
+  answer4.setAttribute('answer', 3);
+  const spans4 = answer4.getElementsByTagName('span');
+  const span4 = spans4[1];
+  span4.innerHTML = decodeURIComponent(escape(answers[3]));
+}
+
+const evaluateAnswer = (answer) => {
+  const level = parseInt(answer.getAttribute('level'));
+  const ans = parseInt(answer.getAttribute('answer'));
+  const qSelected = data.games[challengeSelected].questions[level];
+
+  if (qSelected.correct === ans) nextLevel(answer);
+  else gameOver(answer);
+}
+
+const gameOver = (answer) => {
+  startSound('wrongsound', 0);
+  answer.style.backgroundColor = 'red';
+  gameOverEl.innerHTML = `Perdiste ${localStorage.getItem('name')}`;
+
+  setTimeout(() => {
+    gameOverEl.style.display = 'block';
+    game.style.display = 'none';
+  }, 1000);
+
+  localStorage.removeItem('name');
+  localStorage.removeItem('challenge');
+}
+
+const nextLevel = (answer) => {
+  const level = parseInt(answer.getAttribute('level'));
+  startSound('rightsound', 0);
+  answer.style.backgroundColor = 'green';
+
+  setTimeout(() => {
+    setGame(data.games[challengeSelected], level + 1);
+    answer.style.backgroundColor = 'transparent';
+  }, 1000);
+
+  document.getElementById(`price-${level + 1}`).className = "active";
+}
+
+preStart.style.display = 'block';
+
+const name_ = localStorage.getItem('name');
+const challenge = localStorage.getItem('challenge');
+
+if (!name_ || !challenge) window.location.href = "index.html";
+
+start.onclick = function () {
+  // reduscir uno porque es un arreglo
+  let index = parseInt(challenge) - 1;
+  challengeSelected = index;
+
+  setGame(data.games[index], 0);
+
+  preStart.style.display = 'none';
+  startSound('background', true);
+  game.style.display = 'block';
+}
+
+answer1.onclick = () => { evaluateAnswer(answer1) }
+answer2.onclick = () => { evaluateAnswer(answer2) }
+answer3.onclick = () => { evaluateAnswer(answer3) }
+answer4.onclick = () => { evaluateAnswer(answer4) }
